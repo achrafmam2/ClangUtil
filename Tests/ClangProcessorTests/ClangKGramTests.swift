6b57c9a3eb76b02da9842bd36062d0ee096ddab2
achraf <achraf.mamdouh@yahoo.com>
2018-03-06 15:53:18 +0000

    Remove DB functions Separate ClangKGram into a seperate file. Separate DocumentRepresentable into a separate file.

---
+++ Tests/ClangProcessorTests/ClangKGramTests.swift
@@ -0,0 +1,17 @@
+import Foundation
+import XCTest
+import MongoKitten
+@testable import ClangProcessor
+
+
+class ClangKgramTests: XCTestCase {
+  func testDocumentConversion() {
+    let processor = try! ClangProcessor(src: "int main() {}", language: .c)
+    let kgrams = processor.kgrams(tokens: processor.tokens(), windowSize: 6)
+    XCTAssertEqual(kgrams.count, 1)
+    XCTAssertNotNil(kgrams[0].document)
+    XCTAssertEqual(
+      kgrams[0].document!["tokens"] as! String,
+      "keyword identifier punctuation punctuation punctuation punctuation")
+  }
+}
