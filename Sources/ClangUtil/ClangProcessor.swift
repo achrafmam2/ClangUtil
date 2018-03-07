809e9c0af11d5db0459116de9cb5074d610e7d89
achraf <achraf.mamdouh@yahoo.com>
2018-03-07 17:18:04 +0000

    Detect declaratations outside main file using clang_Location_isFromMainFile

--- Sources/ClangUtil/ClangProcessor.swift
+++ Sources/ClangUtil/ClangProcessor.swift
@@ -113,37 +113,6 @@ public class ClangProcessor {
     }
   }
 
-  /// Declarations that are part of the original source file before the first
-  /// pass of the Preprocessor.
-  /// TODO: This fails when translation unit was created from a source file.
-  private lazy var inSourceDeclarations: Set<String> = {
-    let file = unit.spelling
-    let trimmedSrc = removeIncludes(source:
-      try! String(contentsOfFile: file, encoding: .utf8))
-    let tmpUnit = try! TranslationUnit(clangSource: trimmedSrc, language: .c)
-
-    // Allowed top declaration.
-    let allowedDeclarations = [
-      CXCursor_FunctionDecl,
-      CXCursor_VarDecl,
-      CXCursor_TypedefDecl,
-      CXCursor_EnumConstantDecl,
-      CXCursor_StructDecl,
-      CXCursor_UnionDecl,
-    ]
-
-    var declarations = Set<String>()
-    tmpUnit.visitChildren { cursor in
-      let cursorKind = clang_getCursorKind(cursor.asClang())
-      if allowedDeclarations.contains(cursorKind) {
-        declarations.insert(cursor.displayName)
-      }
-      return ChildVisitResult.recurse
-    }
-
-    return declarations
-  }()
-
   /// Function type for cursor filtering.
   /// - Parameter cursor: A Cursor type.
   /// - Returns: True or False.
@@ -185,10 +154,9 @@ public class ClangProcessor {
       // When a code includes a library (e.g., #include <stdio.h>) lot of
       // declarations are brought by the preprocesor which is noise when
       // checking for plagiarism.
-      if let parent = cursor.lexicalParent, parent == unit.cursor {
-        if !inSourceDeclarations.contains(cursor.displayName) {
-          return ChildVisitResult.continue
-        }
+      let location = cursor.range.start.asClang()
+      if clang_Location_isFromMainFile(location) == 0 {
+        return ChildVisitResult.continue
       }
 
       if isIncluded(cursor) {
@@ -213,46 +181,6 @@ public class ClangProcessor {
         clang_getCursorKind(cursor.asClang())
       ).asSwift()
     }
-  }
-
-
-  /// Removes include directives from the file (e.g., #include <stdio.h>).
-  /// Includes are replaced by an empty string.
-  ///
-  /// ### Example: ###
-  /// ````
-  /// 1. #include <stdio.h>
-  /// 2. #include <string.h>
-  /// 3. void main(void) {}
-  /// ````
-  /// Will be converted to:
-  /// ````
-  /// 1.
-  /// 2.
-  /// 3. void main(void) {}
-  /// ````
-  ///
-  ///  - Parameter source: Source code to preprocess.
-  /// - Returns: Source code without include directives.
-  ///
-  /// - Remark: The count of the line numbers will not change.
-  private func removeIncludes(source: String) -> String {
-    // Regex definitions.
-    let whitespaces = "[ \t]*"                   // Matches zero of more whitespaces.
-    let libraryName = "(<.*>" + "|" + "\".*\")"  // Matches <.*> or ".*"
-
-    // Matches include directives.
-    let pattern = "#include\(whitespaces)\(libraryName)"
-
-    let regex = try! NSRegularExpression(pattern: pattern)
-    return regex.stringByReplacingMatches(in: source,
-                                          options: [],
-                                          range: self.fullTextRange(in: source),
-                                          withTemplate: "")
-  }
-
-  private func fullTextRange(in s: String) -> NSRange {
-    return NSRange(location: 0, length: s.utf8.count)
   }
 }
 
